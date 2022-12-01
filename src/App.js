5d2429288312dbcadea23a3b33a241f5e28051f4
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-02 01:38:35 +0600

    login and spotify token updates according to token

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React,{useEffect} from 'react';
 import Login from './Components/Login';
+import Spotify from './Components/Spotify';
 import { reducerCases } from './utils/Constants';
 import { useStateProvider } from './utils/StateProvider';
 
@@ -10,13 +11,13 @@ function App() {
    const hash = window.location.hash;
    if(hash){
     const token = hash.substring(1).split("&")[0].split('=')[1];
-    dispatch({action: reducerCases.SET_TOKEN, token})
+    dispatch({type: reducerCases.SET_TOKEN, token})
    }
   },[token, dispatch]);
 
   return (
     <div>
-      <Login />
+      {token ? <Spotify /> : <Login />}
     </div>
   )
 }
