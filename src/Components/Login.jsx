6b4b15237b117b0df4ba7a4bdff836d19bb3f714
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-01 19:35:31 +0600

    login updates

---
+++ src/Components/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styledComponents from 'styled-components';
+
+function Login() {
+  return (
+    <Container>
+        <img src="" alt="" />
+        <button>Connect Spotify</button>
+    </Container>
+  )
+}
+
+export default Login;
+
+const Container = styledComponents.div``;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { StateProvider } from './Components/StateProvider';
-import reducer from './Components/reducer';
+import reducer, { initialState } from './utils/reducer';
+import { StateProvider } from './utils/StateProvider';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
