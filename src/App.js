6825a04e72ee160f5563ababd9a024b0dde318aa
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-02 01:23:41 +0600

    stateprovider to app js file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React,{useEffect} from 'react'
-import Login from './Components/Login'
+import React,{useEffect} from 'react';
+import Login from './Components/Login';
+import { reducerCases } from './utils/Constants';
+import { useStateProvider } from './utils/StateProvider';
 
 function App() {
+  const [{ token }, dispatch] = useStateProvider();
 
   useEffect(() => {
    const hash = window.location.hash;
    if(hash){
     const token = hash.substring(1).split("&")[0].split('=')[1];
-    console.log(token);
+    dispatch({action: reducerCases.SET_TOKEN, token})
    }
-  },[]);
+  },[token, dispatch]);
 
   return (
     <div>
---
+++ src/Components/Spotify.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Spotify() {
+  return (
+    <div>Spotify</div>
+  )
+}
+
+export default Spotify
