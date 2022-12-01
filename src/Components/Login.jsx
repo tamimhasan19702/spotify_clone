46c0a09d32fc9e4e7520b20b45c71af5660a936e
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-01 22:52:51 +0600

    added logo and styleing with styled components

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -4,7 +4,7 @@ import styledComponents from 'styled-components';
 function Login() {
   return (
     <Container>
-        <img src="" alt="" />
+        <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_Cmyk_Black.png" alt="Spotify" />
         <button>Connect Spotify</button>
     </Container>
   )
