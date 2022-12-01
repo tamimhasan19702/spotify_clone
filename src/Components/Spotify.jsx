f49b9ae3986b96543174c200ee8ebb4e56ca7874
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-02 02:22:47 +0600

    spotify component styled

--- src/Components/Spotify.jsx
+++ src/Components/Spotify.jsx
@@ -27,5 +27,22 @@ function Spotify() {
 export default Spotify
 
 const Container = styledComponents.div`
-
+max-width: 100vw;
+max-height: 100vh;
+overflow: hidden;
+display: grid;
+grid-template-rows: 85vh 15vh;
+.spotify-body{
+    display: grid;
+    grid-template-columns: 15vw 85vw;
+    height: 100%;
+    width: 100%;
+    background: linear-gradient(transparent, rgba(0,0,0,1));
+    background-color: rgb(32,87,100);
+    .body{
+        height: 100%;
+        width: 100%;
+        overflow: auto;
+    }
+}
 `
