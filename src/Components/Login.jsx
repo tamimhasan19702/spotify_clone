df724bfe18ffbfcfa9ec481c7d7b399bd019ea22
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-01 23:57:26 +0600

    api variables declared

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -3,10 +3,29 @@ import styled from 'styled-components';
 import logoImg from '../imgs/Spotify_Logo_CMYK_Black.png';
 
 function Login() {
+  
+  const handleClick = () => {
+  
+   const clientId = 'd0551278b7f04adb803a8db613a4e553';
+   const redirectUrl = 'http://localhost:3000/';
+   const apiUrl = 'https://accounts.spotify.com/authorize';
+   const scope = [
+    "user-read-email",
+    "user-read-private",
+    "user-modify-playback-state",
+    "user-read-playback-state",
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-position",
+    "user-top-read",
+   ];
+  window.location.href = `$`
+  }
+
   return (
     <Container>
         <img src={logoImg} alt="Spotify" />
-        <button>Connect Spotify</button>
+        <button onClick={handleClick}>Connect Spotify</button>
     </Container>
   )
 }
@@ -23,12 +42,16 @@ width: 100vw;
 background-color: #1db954;
 gap: 5rem;
 
-img{
+ img{
   height:20vh;
 }
-
-button{
+ button{
   padding: 1rem 5rem;
   border-radius: 5rem;
+  border: none;
+  background-color: black;
+  color: #49f585;
+  font-size: 1.4rem;
+  cursor: pointer;
 }
 `;
