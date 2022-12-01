c5299c994c864019fc5d0e0fe5726bd52d6e045a
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-02 02:09:31 +0600

    added components to spotify

--- src/Components/Spotify.jsx
+++ src/Components/Spotify.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import styledComponents from 'styled-components';
+import Navbar from './Navbar';
+import Sidebar from './Sidebar';
+import Body from './Body';
+import Footer from './Footer';
 
 function Spotify() {
   return (
-    <div>Spotify</div>
+    <Container>
+        <div className="spotify-body">
+            <Sidebar/>
+            <div className="body">
+                <Navbar/>
+                <div className="body-contents">
+                 <Body />
+                </div>
+            </div>
+        </div>
+        <div className="spotify-footer">
+         <Footer/>
+        </div>
+    </Container>
   )
 }
 
 export default Spotify
+
+const Container = styledComponents.div`
+
+`
