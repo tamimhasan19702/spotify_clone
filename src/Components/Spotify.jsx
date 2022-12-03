0e5948238f63ed8bef23b0f4354ef77943dab333
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-03 19:08:54 +0600

    userinfo collected

--- src/Components/Spotify.jsx
+++ src/Components/Spotify.jsx
@@ -5,14 +5,28 @@ import Sidebar from './Sidebar';
 import Body from './Body';
 import Footer from './Footer';
 import axios from 'axios';
-import styledComponents from 'styled-components';
 import { useStateProvider } from '../utils/StateProvider';
+import { reducerCases } from '../utils/Constants';
 
 function Spotify() {
     const [{token}, dispatch] = useStateProvider();
 
    useEffect(() => {
-   
+   const getUserInfo = async () => {
+    const {data} = await axios.get("https://api.spotify.com/v1/me", {
+        headers:{
+            Authorization: "Bearer " + token,
+            "Content-Type": "application/json",
+        },
+    });
+    const userInfo = {
+        userId: data.id,
+        userUrl: data.external_urls.spotify,
+        userName: data.display_name,
+       };
+       dispatch({type: reducerCases.SET_USER, userInfo})
+   };
+   getUserInfo();
    },[dispatch,token])
     
   return (
