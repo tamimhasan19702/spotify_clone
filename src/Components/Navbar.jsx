ae7cbd9dfaf522c336c222795e76f32736bc336d
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-03 19:39:04 +0600

    Navbar updates

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, {useEffect} from 'react';
+import styledComponents from 'styled-components';
+import { FaSearch } from 'react-icons/fa';
+import { CgProfile } from 'react-icons/cg';
+import axios from 'axios';
+import { MdGeneratingTokens } from 'react-icons/md';
+import { reducerCases } from '../utils/Constants';
 
 function Navbar() {
+
+  useEffect(() => {
+
+  const getUserInfo = async () => {
+    const { data } = await axios.get('https://api.spotify.com/v1/me',{
+      headers : {
+        Authorization: "Bearer " + MdGeneratingTokens,
+        "Content-Type": "application/json",
+      },
+    });
+    
+    const userInfo = {
+      userId: data.id,
+      userName: data.display_name,
+    };
+    dispatch({ type: reducerCases.SET_USER, userInfo});
+  };
+
+getUserInfo();
+
+},[dispatch, token]);
+
+
   return (
-    <div>
-      Navbar
-    </div>
+    <Container>
+      <div className="search-bar">
+        <FaSearch/>
+        <input type="text" placeholder='Artists, songs, or Products'/>
+      </div>
+      <div className="avatar">
+        <a href="#">
+          <CgProfile/>
+          <span>{userInfo?.name}</span>
+        </a>
+      </div>
+    </Container>
   )
 }
 
 export default Navbar
+
+const Container = styledComponents.div``;
