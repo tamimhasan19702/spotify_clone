98919f6c1c9c643006308a9e332b6334cac40b15
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-04 19:50:26 +0600

    Styling Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -30,4 +30,31 @@ console.log({ userInfo });
 
 export default Navbar
 
-const Container = styledComponents.div``;
+const Container = styledComponents.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+padding: 2rem;
+height: 15vh;
+position: sticky;
+top: 0;
+transition: 0.3s ease-in-out;
+background-color: none;
+.search-bar{
+  background-color: white;
+  width: 30%;
+  padding: 0.4rem 1rem;
+  border-radius: 2rem;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  input{
+    border: none;
+    height: 2rem;
+    width: 100%;
+  &:focus{
+    outline: none;
+  }
+  }
+  
+}`;
