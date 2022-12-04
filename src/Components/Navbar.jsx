d3538cdabdea90c3e75bac0bad994966a2a817ca
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-04 20:17:49 +0600

    Navbar style updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -55,6 +55,30 @@ background-color: none;
   &:focus{
     outline: none;
   }
+  } 
+}
+.avatar{
+  background-color: black;
+  padding: 0.3rem 0.4rem;
+  padding-right: 1rem;
+  border-radius: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  a{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 0.5rem;
+    text-decoration: none;
+    color: white;
+    font-weight: bold;
+    svg{
+      font-size: 1.3rem;
+      background-color: #282828;
+      padding: 0.2rem;
+      border-radius: 1rem;
+      color: #e7e5e5;
+    }
   }
-  
 }`;
